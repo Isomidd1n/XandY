c39b19889a07c0eb82a222add80c5791dd363f56
Isomiddin <isomiddinhusniddinov21@gmail.com>
2022-06-18 11:33:27 +0500

    PrintXY

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Point point = new Point();
+        point.x = 45;
+        point.y = 56;
+
+        point.printXY();
+        System.out.println("Koordinatalar (" + point.x + ", " + point.y + ")");
+    }
+}
---
+++ src/Point.java
@@ -0,0 +1,9 @@
+public class Point {
+    int x;
+    int y;
+
+
+    public void printXY(){
+        System.out.println("X: " + x + "\nY: " + y);
+    }
+}
